be6068145c6585e9ce5fa088fafbc8304816e21a
kczulko <karol.czulkowski@protonmail.com>
2021-12-07 10:17:30 +0100

    Beginning of chapter 9

--- chapters/Excs9.hs
+++ chapters/Excs9.hs
@@ -1,1 +1,8 @@
 module Excs9 where
+
+-- >>> subs [1,2,3]
+-- [[],[3],[2],[2,3],[1],[1,3],[1,2],[1,2,3]]
+subs :: [a] -> [[a]]
+subs [] = [[]]
+subs (x:xs) = currentSubs ++ map (x:) currentSubs
+  where currentSubs = subs xs
